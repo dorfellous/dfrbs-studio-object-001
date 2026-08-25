ca5c6bbc4adebe9c509fdd27372732b99d03ab65
dorfellous <dorfellous5@gmail.com>
2026-08-25 16:42:58 +0300

    Configure Vite base path for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/dfrbs-studio-object-001/",
   build: {
     outDir: "dist/client",
   },
